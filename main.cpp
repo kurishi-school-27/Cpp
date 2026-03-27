6fe3bae527cdab862de4bcd4a04d0966ac8a6720
Chris <cplanck@my365.bellevue.edu>
2026-03-26 23:53:00 -0500

    init commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std
+
+int main() {
+    int number
+
+    cout << "Enter a number: " << endl;
+    cin >> number
+
+    if (number > 0) {
+        cout << "Positive number" << endl
+    else
+        cout << "Non-positive number" << endl;
+
+    return 0
+}
